e2a20c3867b6ef54f9867503317d82af4fe616bc
irrelon <rob@irrelon.com>
2016-02-17 13:14:21 +0000

    Added ability to time individual steps within a test

--- test.js
+++ test.js
@@ -1,6 +1,12 @@
 var TB = require('./index');
 
 TB.test('My test test', function (callback) {
+	TB.time('My time step');
+	for (var i = 0; i < 1000000; i++) {
+
+	}
+	TB.time('My time step');
+
 	callback();
 });
 
